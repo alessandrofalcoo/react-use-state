d175eb0244d4903413f94a7dfbeef111bd1ef80d
alessandrofalcoo <alefalco.1003@gmail.com>
2025-03-25 17:24:44 +0100

    add state management for language and description in Main component

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,13 +1,23 @@
 import Button from './Buttons'
 import Languages from '../data/Languages'
+import { useState } from 'react'
 
 export default function Main() {
 
+    const [language, setLanguage] = useState(0)
+    const [description, setDescription] = useState(0)
+
     return (
-        <div className='buttons'>
-            {Languages.map((element) => (
-                <Button id={element.id} title={element.title} >{element}</Button>
-            ))}
-        </div>
+        <>
+            <div className='buttons'>
+                {Languages.map((element) => (
+                    <Button id={element.id} title={element.title}  >{element}</Button>
+                ))}
+            </div>
+            <div className='paragraph'>
+                <h3>{language}</h3>
+                <p>{description}</p>
+            </div>
+        </>
     )
 }
